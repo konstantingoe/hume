e4c91aef17dc07ff9de9d3274be29f383c87e1d7
konstantingoe <konstantin.goebler@tum.de>
2022-06-09 18:44:41 +0200

    add edgenumber function

---
+++ R/edgenumber.R
@@ -0,0 +1,14 @@
+
+#' @title Edgenumber
+#' @description Calculates number of edges given some precision matrix. Thresholding is allowed but defaulted to 0.
+#'
+#' @param Precision a square matrix whose entries are equal to negative partial correlations.
+#' @param cut thresholding parameter when entries are treated to be non-zero.
+#'
+#' @return a number corresponding to the number of edges present in the underlying graph.
+#' @export
+#'
+#' @examples
+edgenumber <- function(Precision=Precision, cut=0){
+  sum((abs(Precision) > (0 + cut))[lower.tri((abs(Precision) > (0 + cut)))])
+}
